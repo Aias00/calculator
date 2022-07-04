9bd279a09c73c660809852672ec948dcd7763e30
liuhy <rokkki@163.com>
2022-07-04 21:46:52 +0800

    update sql

--- calculator.go
+++ calculator.go
@@ -1,14 +1,18 @@
 package calculator
 
+import "fmt"
+
 var logMessage = "[LOG]"
 
 // Version of the calculator
 var Version = "1.0"
 
 func internalSum(number int) int {
+
 	return number - 1
 }
 
 func Sum(number1, number2 int) int {
+	fmt.Println("加法")
 	return number1 + number2
 }
